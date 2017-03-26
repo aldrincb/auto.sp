95c4d962dcf4fa00f23cb3e38fb084d942cd5722
Aldrin Balisi <acbalisi@uwaterloo.ca>
2017-03-25 20:09:09 -0400

    Fixed stuff

--- autosp.py
+++ autosp.py
@@ -6,24 +6,22 @@ cars = []
 not_cars = []
 
 # Get car paths
-car_paths = ["/udacity-dataset/vehicles/GTI_Far",
-"/udacity-dataset/vehicles/GTI_Left",
-"/udacity-dataset/vehicles/GTI_MiddleClose",
-"/udacity-dataset/vehicles/GTI_Right",
-"/udacity-dataset/vehicles/KITTI_extracted"]
+car_paths = ["./udacity-dataset/vehicles/GTI_Far"
+"./udacity-dataset/vehicles/GTI_Left",
+"./udacity-dataset/vehicles/GTI_MiddleClose",
+"./udacity-dataset/vehicles/GTI_Right",
+"./udacity-dataset/vehicles/KITTI_extracted"]
 
 for i in xrange(0, len(car_paths)):
     car_path = car_paths[i]
-    os.chdir(car_path)
-    for file in glob.glob("*.png"):
+    for file in glob.glob(os.path.join(car_path, "*.png")):
         cars.append(file)
 
 # Get not car paths
-not_car_paths = ["/udacity-dataset/non-vehicles/Extras",
-"/udacity-dataset/non-vehicles/GTI"]
+not_car_paths = ["./udacity-dataset/non-vehicles/Extras",
+"./udacity-dataset/non-vehicles/GTI"]
 
 for i in xrange(0, len(not_car_paths)):
     not_car_path = not_car_paths[i]
-    os.chdir(not_car_path)
-    for file in glob.glob("*.png"):
+    for file in glob.glob(os.path.join(not_car_path, "*.png")):
         not_cars.append(file)
